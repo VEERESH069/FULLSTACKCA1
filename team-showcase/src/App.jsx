23f9753b83e64273faac7ea3eefd89ef2357b462
VEERESH069 <116574954+VEERESH069@users.noreply.github.com>
2025-02-27 12:01:49 +0530

    done

---
+++ team-showcase/src/App.jsx
@@ -0,0 +1,13 @@
+import TeamMemberCard from './component/TeamMemberCard';
+
+function App() {
+  return (
+    <>
+      <h1>Our Team</h1>
+      <TeamMemberCard name="Veeresh S K" jobTitle="Lead Software Engineer" />
+      <TeamMemberCard name="John Doe" jobTitle="Software Engineer" />
+    </>
+  );
+}
+
+export default App;
---
+++ team-showcase/src/component/TeamMemberCard.jsx
@@ -0,0 +1,10 @@
+const TeamMemberCard = ({ name, jobTitle }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{jobTitle}</p>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
